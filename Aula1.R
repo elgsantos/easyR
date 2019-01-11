b15d464da6825d0b17b6e9c52bae8f32c05303cf
Eduardo Gomes <elgsantos@gmail.com>
2019-01-10 23:55:48 -0200

    Gráficos e dados adicionados

--- Aula1.R
+++ Aula1.R
@@ -1,9 +1,9 @@
 #Mudar caminho pasta
-setwd("~/EasyR")
+setwd("~/EasyR/minicurso")
 getwd()
 
 #Ler csv sep separador, dec decimal
-env <- read.csv("~/EasyR/Doubs.csv",header=T,sep=",",dec=".")
+env <- read.csv("~/EasyR/minicurso/Doubs.csv",header=T,sep=",",dec=".")
 env
 
 #dataframe objeto bidimensional que suporta dados de dif tipos
@@ -34,3 +34,53 @@ summary(env[8:10])
 range(env)
 range(env$nit)
 max(env$nit)
+
+#tipologia 4 zonas, partindo da nascente
+#zona da truta, salmo trutta fario
+#zona do grayling, thymallus thymallus
+#zona do barbo, brabus barbus
+#zona da brema, abramis brama
+
+#2primeiros
+#Regiao dos Salmonídeos: bem oxigenada e oligotrófica
+#2ultimos
+#Regiao dos Ciprinideos: eutrófica e baixa concentração de oxy
+
+#análise exploratória para identificar a região
+#utilizar oxigênio, nitrato e amônio x das(km)
+#altidute(m) x das (km)
+
+#funcao para salvar figura
+jpeg("~/EasyR/minicurso/Variaveis.jpg",width = 200,height = 200, bg="white", res=300,
+     units="mm")
+#par - funcao de parametros graficos
+#multiframerow mfrow
+par(mfrow=c(2,2),mar=c(4.5,4.5,1,1))#inferior,esq,sup,dir
+plot(x=env$das,y=env$nit,type="l",xlab="distância da fonte(km)",
+     ylab="Nitrato (mg/L)",col="red",lwd=2)
+plot(x=env$das,y=env$amm,type="l",xlab="distância da fonte(km)",
+     ylab="Amônio (mg/L)",col="green",lwd=2)
+plot(x=env$das,y=env$oxy,type="l",xlab="distância da fonte(km)",
+     ylab="Oxigênio (mg/L)",col="orange",lwd=2)
+plot(x=env$das,y=env$alt,type="l",xlab="distância da fonte(km)",
+     ylab="Altitude (m))",col="blue",lwd=2)
+points(x=env$das[env$zona=="Truta"],y=env$alt[env$zona=="Truta"],
+       pch=19,col="black")
+points(x=env$das[env$zona=="Grayling"],y=env$alt[env$zona=="Grayling"],
+       pch=19,col="gray40")
+points(x=env$das[env$zona=="Barbo"],y=env$alt[env$zona=="Barbo"],
+       pch=19,col="gray70")
+points(x=env$das[env$zona=="Brema"],y=env$alt[env$zona=="Brema"],
+       pch=21,col="black",bg="white")
+legend("topright",legend=c("Truta","Grayling","Barbo","Brema"),
+       bty="n",pch=c(19,19,19,21),col=c("black","gray40","gray70","black"),
+       pt.cex=1.2,title="Zonas")
+abline(v=200,lty=3,col="darkorange",lwd=2)
+text(x=200,y=300,labels="Região \nSalmonídeo",pos=2, col="darkorange")
+text(x=200,y=400,labels="Região \nCiprinídeo",pos=2, col="darkorange")
+
+
+
+#salvar
+dev.off()
+
